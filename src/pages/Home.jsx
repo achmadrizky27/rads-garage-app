b79a6dffad91971b4db36526d0ce0af91486c53b
achmadrizky27 <rizkyach27@gmail.com>
2026-05-09 17:56:20 +0700

    Update: app.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,19 +1,19 @@
-import { useEffect } from 'react';
-import { useReveal } from '../hooks/useReveal';
-import Hero from '../components/Hero';
-import About from '../components/About';
-import Services from '../components/Services';
-import Stats from '../components/Stats';
-import Gallery from '../components/Gallery';
-import Team from '../components/Team';
-import Testimonials from '../components/Testimonials';
-import Contact from '../components/Contact';
+import { useEffect } from "react";
+import { useReveal } from "../hooks/useReveal";
+import Hero from "../components/Hero";
+import About from "../components/About";
+import Services from "../components/Services";
+import Stats from "../components/Stats";
+import Gallery from "../components/Gallery";
+import Team from "../components/Team";
+import Testimonials from "../components/Testimonials";
+import Contact from "../components/Contact";
 
 export default function Home() {
   useReveal();
 
   return (
-    <main>
+    <main className="overflow-x-hidden">
       <Hero />
       <About />
       <Services />
